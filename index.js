c79ad0cc3b497315e780fd3f27fb75bb24f6b6d4
ayanbajo <bolatitoayanbajo@gmail.com>
2022-07-21 21:32:31 -0400

    Started project: created html, js, and css files

---
+++ index.js
@@ -0,0 +1,242 @@
+const horrorQuestion = [
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+    {
+        question: "",
+        answers: {
+            a: "",
+            b: "",
+            c: "",
+        },
+        correctAnswer: ""
+    },
+
+]
+
